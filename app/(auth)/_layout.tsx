05ffae0b42084de3d7e432d9db8b34cab86f7222
Rahul Kardile <rahulkardile321@gmail.com>
2026-05-11 23:02:13 +0530

    updated the safeareaview and styles

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,6 @@
-import "@/global.css";
-import { Stack } from "expo-router";
+// import "@/global.css";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
+// export default function RootLayout() {
+//   return <Stack screenOptions={{ headerShown: false }} />;
+// }
